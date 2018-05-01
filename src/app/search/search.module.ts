1eecd0be767375e35146ba10a6aaee90b549ae82
joseph slater <>
2018-05-01 11:50:42 -0500

    updates scss and adds the search module

---
+++ src/app/search/search.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SearchComponent } from './search/search.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [SearchComponent]
+})
+export class SearchModule { }
