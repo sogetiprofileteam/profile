68f499be134f91e5fad959cb6d33aeb393277554
joseph slater <>
2018-05-01 17:26:59 -0500

    adds app folder and adds spacing variable

--- src/app/app/app.module.ts
+++ src/app/app/app.module.ts
@@ -2,10 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { SearchComponent } from './search/search.component';
-import { PortfolioItemComponent } from './portfolio-item/portfolio-item.component';
-import { SearchModule } from './search/search.module';
-import { PortfolioItemModule } from './portfolio-item/portfolio-item.module';
+
+import { PortfolioItemModule } from '../portfolio-item/portfolio-item.module';
+import { SearchModule } from '../search/search.module';
 
 
 @NgModule({
