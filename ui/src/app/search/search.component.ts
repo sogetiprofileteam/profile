8a155aa7dcffb70e22fa6267778f3a34f956c4f7
joseph slater <>
2018-05-03 16:15:47 -0500

    updates search

--- ui/src/app/search/search.component.ts
+++ ui/src/app/search/search.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Profile } from '../models/profile';
+
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.scss']
 })
 export class SearchComponent implements OnInit {
+  profile = new Profile();
 
   constructor() { }
 
