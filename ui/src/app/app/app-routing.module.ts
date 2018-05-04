6a1b5ae90f944e4b7bc88295f603adf97fb53068
Joe <josephbarkate@gmail.com>
2018-05-04 11:40:45 -0500

    adding link for profile

--- ui/src/app/app/app-routing.module.ts
+++ ui/src/app/app/app-routing.module.ts
@@ -4,11 +4,16 @@ import { RouterModule, Routes }  from '@angular/router';
 import { AppComponent }   from './app.component';
 import { PortfolioAddComponent }   from '../portfolio-add/portfolio-add.component';
 import { SearchComponent } from '../search/search.component';
+import { PortfolioComponent } from '../portfolio/portfolio.component';
+import { PageNotFoundComponent } from './not-found.component';
 
 const appRoutes: Routes = [
   { path: '', component: AppComponent },
   { path: 'add', component: PortfolioAddComponent },
-  { path: 'search', component: SearchComponent }
+  { path: 'search', component: SearchComponent },
+  { path: 'portfolio' , component: PortfolioComponent },
+  // { path: '**', component: PageNotFoundComponent},  
+  
 ];
 
 @NgModule({
