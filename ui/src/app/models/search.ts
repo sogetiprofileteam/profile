0397607db8f48ef7ffd0ce9ce0885976ae6e05f8
Bryce Oldham <bryceoldham@gmail.com>
2018-05-04 13:17:07 -0500

    updates search service

---
+++ ui/src/app/models/search.ts
@@ -0,0 +1,5 @@
+export class Search {
+    skills: string[];
+    practice: string;
+    ato: boolean;
+  }
--- ui/src/app/search/search.service.ts
+++ ui/src/app/search/search.service.ts
@@ -1,8 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import {Observable} from 'rxjs/Observable';
+import { Profile } from '../models/profile';
+import { Search } from '../models/search';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
 
 @Injectable()
 export class SearchService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getSearchResponse(query: Search) {
+    return this.http.get('https://localhost:3000/search?practice=' + query.practice + '&skill=' + query.skills + '&ato=' + query.ato);
+}
+
 
 }
