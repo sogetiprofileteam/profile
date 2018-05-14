e437d89fed013dd9d4022b12489e1125cadc2734
Larry Foulkrod <lefoulkrod@gmail.com>
2018-05-14 13:18:39 -0500

    Docker-ized the UI

--- ui/src/environments/environment.prod.ts
+++ ui/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 import { HttpHeaders } from '@angular/common/http';
 
 const api = {
-  url: 'http://localhost:3000',
+  url: 'https://sogeti-profile-api-dev.herokuapp.com',
   resources: {
     search: '/search',
     profile: '/profile'
