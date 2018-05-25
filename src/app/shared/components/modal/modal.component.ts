e006c3679c6f6691d5588d0e0add97a88caf26bb
Bryce Oldham <bryceoldham@gmail.com>
2018-05-25 11:06:05 -0500

    adds shared modal component

---
+++ src/app/shared/components/modal/modal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-modal',
+  templateUrl: './modal.component.html',
+  styleUrls: ['./modal.component.scss']
+})
+export class ModalComponent implements OnInit {
+  @Input() id: string;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
