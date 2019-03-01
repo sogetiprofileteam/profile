7797f162e8d16d2e796c74cf44f9814b8f85abc5
Natalie <38986272+NatThrock@users.noreply.github.com>
2019-03-01 14:47:50 -0600

    Add Angular Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -11,6 +12,7 @@ import { CoreModule } from '@core/core.module';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     CoreModule
   ],
