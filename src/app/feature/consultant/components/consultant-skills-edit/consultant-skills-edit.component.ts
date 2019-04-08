ef09d1f5f57f64adcbeec327fa9257d229f24b17
johnsonbrianna <johnsonbrianna1414@gmail.com>
2019-04-08 15:11:33 -0500

    Updated the TS file for the edit-skills component

--- src/app/feature/consultant/components/consultant-skills-edit/consultant-skills-edit.component.ts
+++ src/app/feature/consultant/components/consultant-skills-edit/consultant-skills-edit.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { tap, takeUntil } from 'rxjs/operators';
+import { Subject } from 'rxjs';
+
+import { ConsultantStore } from '@feature/consultant/services/consultant-store/consultant-store.service';
+import { Consultant } from '@core/models';
+
+import { MatDialogRef } from '@angular/material';
 
 @Component({
   selector: 'app-consultant-skills-edit',
@@ -7,7 +15,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConsultantSkillsEditComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private consultantStore: ConsultantStore,
+    private dialogRef: MatDialogRef<ConsultantSkillsEditComponent>,
+    private formBuilder: FormBuilder
+  ) { }
+
+  skillsForm = this.formBuilder.group({
+    search: ['Search for skills (ex: HTML)', Validators.required],
+  });
+
+  consultant$ = this.consultantStore.consultant$.pipe(tap(consultant => this.skillsForm.patchValue(consultant)));
+  destroy$ = new Subject();
+
+  close(): void {
+    this.dialogRef.close();
+  }
+
+  updateConsultant(): void {
+    if (this.skillsForm.valid) {
+      const updatedData = this.getFormData();
+
+      this.consultantStore.updateConsultant(updatedData)
+        .pipe(takeUntil(this.destroy$))
+        .subscribe(() => this.close());
+    }
+  }
+
+  getFormData(): Partial<Consultant> {
+    return this.skillsForm.value as Partial<Consultant>;
+  }
 
   ngOnInit() {
   }
