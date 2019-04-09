e285b802caa1e21b890894a67b2c1726b54b06e9
johnsonbrianna <johnsonbrianna1414@gmail.com>
2019-04-09 13:44:01 -0500

    Edit Skills

--- src/app/feature/consultant/components/consultant-skills-edit/consultant-skills-edit.component.ts
+++ src/app/feature/consultant/components/consultant-skills-edit/consultant-skills-edit.component.ts
@@ -9,45 +9,75 @@ import { Consultant } from '@core/models';
 import { MatDialogRef } from '@angular/material';
 
 import {COMMA, ENTER} from '@angular/cdk/keycodes';
-import {MatChipInputEvent} from '@angular/material';
+import {ElementRef, ViewChild} from '@angular/core';
+import {FormControl} from '@angular/forms';
+import {MatAutocompleteSelectedEvent, MatChipInputEvent, MatAutocomplete} from '@angular/material';
+import {Observable} from 'rxjs';
+import {map, startWith} from 'rxjs/operators';
 
 export interface Skill {
   name: string;
 }
 
-export class ChipsInput {
+export class ChipsAutocompleteExample {
   visible = true;
   selectable = true;
   removable = true;
   addOnBlur = true;
-  readonly separatorKeysCodes: number[] = [ENTER, COMMA];
-  skill: Skill[] = [
-    {name: 'UX Designer'},
-    {name: 'UI Designer'},
-    {name: 'Teamwork'},
-  ];
+  separatorKeysCodes: number[] = [ENTER, COMMA];
+  skillCtrl = new FormControl();
+  filteredSkills: Observable<string[]>;
+  skills: string[] = ['Teamwork'];
+  allSkills: string[] = ['Communication', 'SDLC', 'Teamwork', 'UI Designer', 'UI Designer'];
+
+  @ViewChild('skillInput') skillInput: ElementRef<HTMLInputElement>;
+  @ViewChild('auto') matAutocomplete: MatAutocomplete;
+
+  constructor() {
+    this.filteredSkills = this.skillCtrl.valueChanges.pipe(
+        startWith(null),
+        map((skill: string | null) => skill ? this._filter(skill) : this.allSkills.slice()));
+  }
 
   add(event: MatChipInputEvent): void {
-    const input = event.input;
-    const value = event.value;
+    // Add skill only when MatAutocomplete is not open
+    // To make sure this does not conflict with OptionSelected Event
+    if (!this.matAutocomplete.isOpen) {
+      const input = event.input;
+      const value = event.value;
 
-    // Add our 
-    if ((value || '').trim()) {
-      this.skill.push({name: value.trim()});
-    }
+      // Add our skill
+      if ((value || '').trim()) {
+        this.skills.push(value.trim());
+      }
 
-    // Reset the input value
-    if (input) {
-      input.value = '';
+      // Reset the input value
+      if (input) {
+        input.value = '';
+      }
+
+      this.skillCtrl.setValue(null);
     }
   }
 
-  remove(fruit: Skill): void {
-    const index = this.skill.indexOf(fruit);
+  remove(skill: string): void {
+    const index = this.skills.indexOf(skill);
 
     if (index >= 0) {
-      this.skill.splice(index, 1);
+      this.skills.splice(index, 1);
     }
+  }
+
+  selected(event: MatAutocompleteSelectedEvent): void {
+    this.skills.push(event.option.viewValue);
+    this.skillInput.nativeElement.value = '';
+    this.skillCtrl.setValue(null);
+  }
+
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+
+    return this.allSkills.filter(skill => skill.toLowerCase().indexOf(filterValue) === 0);
   }
 }
 
