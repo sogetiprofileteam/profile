dff2da9c6557de990c3f9f64bd961b86e537ceb1
Natalie <38986272+NatThrock@users.noreply.github.com>
2019-04-18 14:00:59 -0500

    Cleanup

--- src/app/feature/consultant/components/shared/consultant-skills-edit/consultant-skills-edit.component.ts
+++ src/app/feature/consultant/components/shared/consultant-skills-edit/consultant-skills-edit.component.ts
@@ -1,4 +1,4 @@
-import { Component, ChangeDetectionStrategy, OnDestroy, ElementRef, ViewChild, Inject, OnInit } from '@angular/core';
+import { Component, ChangeDetectionStrategy, OnDestroy, ElementRef, ViewChild, Inject } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { COMMA, ENTER } from '@angular/cdk/keycodes';
 
@@ -11,7 +11,7 @@ import { Skill } from '@core/models';
 import { ConsultantStore } from '@feature/consultant/services/consultant-store/consultant-store.service';
 import { ConsultantSkillDataService } from '@feature/consultant/services/consultant-skill-data/consultant-skill-data.service';
 
-import { cloneDeep, isEqual, differenceWith } from 'lodash';
+import { isEqual, differenceWith } from 'lodash';
 
 export interface SkillsEditDialogData {
   type: SkillsType
@@ -29,7 +29,7 @@ export type SkillsType = 'coreSkills' | 'technicalSkills';
   styleUrls: ['./consultant-skills-edit.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ConsultantSkillsEditComponent implements OnInit, OnDestroy {
+export class ConsultantSkillsEditComponent implements OnDestroy {
   @ViewChild('skillInput') skillInput: ElementRef<HTMLInputElement>;
   @ViewChild('auto') matAutocomplete: MatAutocomplete;
 
@@ -44,33 +44,11 @@ export class ConsultantSkillsEditComponent implements OnInit, OnDestroy {
   separatorKeysCodes: number[] = [ENTER, COMMA];
   skillCtrl = new FormControl();
 
-  filteredAvailableSkills$ = this.skillCtrl.valueChanges.pipe(
-    startWith(null),
-    // This could be a string or Skill because the mat-chip value is the Skill object itself,
-    // so when one selects or enters a skill the inputbox valueChanges could emit a string or 
-    // Skill object. This isn't as clean as I'd like it to be.
-    mergeMap((value: string | Skill) => {
-      if (value) {
-        let name: string;
-
-        if (typeof value !== 'string') {
-          name = value.name;
-        } else {
-          name = value;
-        }
-
-        return this.filterSkills(name);
-      } else {
-        return this.availableSkills$;
-      }
-    })
-  );
-
   destroy$ = new Subject();
   consultant$ =
     this.consultantStore.consultant$
       .pipe(
-        tap(c => this._selectedSkills$.next(c[this.skillType]))
+        tap(consultant => this._selectedSkills$.next(consultant[this.skillType]))
       );
 
   getSkills$ =
@@ -78,31 +56,74 @@ export class ConsultantSkillsEditComponent implements OnInit, OnDestroy {
       ? this.consultantSkillService.getCoreSkills().pipe(takeUntil(this.destroy$))
       : this.consultantSkillService.getTechnicalSkills().pipe(takeUntil(this.destroy$))
 
+  // _selectedSkills$ is BehaviorSubject because we want to be able to manipulate
+  // skills as the user interacts with the application. If it were a basic Observable 
+  // then we couldn't manipulate the skills over time. 
   private _selectedSkills$: BehaviorSubject<Skill[]> = new BehaviorSubject(null);
-  selectedSkills$ = this._selectedSkills$.asObservable().pipe(map(skills => skills.sort(this.dynamicSort('name'))));
-  get selectedSkills(): Skill[] {
+  
+  // We want to watch for changes in the _selectedSkills$ BehaviorSubject hence turning
+  // it into an Observable stream.
+  selectedSkills$ = 
+    this._selectedSkills$.asObservable()
+      .pipe(
+        map(skills => skills.sort(this.dynamicSort('name')))
+      );
+  
+  // We need to access the currently selectedSkills when using .next() on the _selectedSkills$
+  // BehaviorSubject because we can't just push new values into an array, we have to .next() and
+  // entire array. This will often be used in conjunction with the spread operator `...` to copy 
+  // the values.
+  private get selectedSkills(): Skill[] {
     return this._selectedSkills$.value;
   }
 
   availableSkills$: Observable<SkillOption[]> =
     combineLatest(this.selectedSkills$, this.getSkills$)
       .pipe(
+        // We need to know which skills are in the selectedSkills Observable so we can
+        // disable them in the dropdown options to prevent duplicates.
         map(([selectedSkills, availableSkills]) => {
           return this.buildAvailableSkillsOptions(selectedSkills, availableSkills);
         })
       )
 
+  filteredAvailableSkills$ = 
+    this.skillCtrl.valueChanges
+      .pipe(
+        startWith(null),
+        // This could be a string or Skill because the mat-chip value is the Skill object itself,
+        // so when one selects or enters a skill the inputbox valueChanges could emit a string or 
+        // Skill object. This isn't as clean as I'd like it to be.
+        mergeMap((value: string | Skill) => {
+          if (value) {
+            let name: string;
+
+            if (typeof value !== 'string') {
+              name = value.name;
+            } else {
+              name = value;
+            }
+
+            return this.filterSkills(name);
+          } else {
+            return this.availableSkills$;
+          }
+        })
+      );  
+
   private buildAvailableSkillsOptions(selectedSkills: Skill[], availableSkills: Skill[]) {
+    // Add the selected boolean to selectedSkills, ignore skills with null ID because those
+    // were entered via freetext and shouldn't be added to the avialableSkills until added to DB
     const selectedSkillsOptions = selectedSkills.map(skill => {
       const selectedSkill: SkillOption = {
         ...skill,
         selected: true
       };
       return selectedSkill;
-      // Filter out skills with null ID because those were entered via freetext and 
-      // shouldn't be added to the avialableSkills until added to the DB
     }).filter(skill => skill.id !== null);
 
+    // Similar to above but we get the items that aren't inside the selectedSkills array
+    // Lodash's differenceWith() and isEqual() are a godsend here
     const unselectedSkills: Skill[] = differenceWith(availableSkills, selectedSkills, isEqual);
     const unselectedSkillsOptions = unselectedSkills.map(skill => {
       const selectedSkill: SkillOption = {
@@ -112,6 +133,7 @@ export class ConsultantSkillsEditComponent implements OnInit, OnDestroy {
       return selectedSkill;
     });
 
+    // Merge all options (might be able to use Lodash's merge() function)
     const availableSkillsOptions: SkillOption[] = [
       ...selectedSkillsOptions,
       ...unselectedSkillsOptions
@@ -120,7 +142,14 @@ export class ConsultantSkillsEditComponent implements OnInit, OnDestroy {
     return availableSkillsOptions.sort(this.dynamicSort('name'))  ;
   }
 
-  dynamicSort(property: string) {
+  ngOnDestroy() {
+    this.destroy$.next();
+  }
+
+  // Sort any array of objects by a property name.
+  // At time of writing this is from the second response here: 
+  // https://stackoverflow.com/questions/1129216/sort-array-of-objects-by-string-property-value
+  private dynamicSort(property: string) {
     let sortOrder = 1;
 
     if(property[0] === "-") {
@@ -132,14 +161,6 @@ export class ConsultantSkillsEditComponent implements OnInit, OnDestroy {
         let result = (a[property] < b[property]) ? -1 : (a[property] > b[property]) ? 1 : 0;
         return result * sortOrder;
     }
-  }
-
-  ngOnInit() {
-    
-  }
-
-  ngOnDestroy() {
-    this.destroy$.next();
   }
 
   close(): void {
@@ -180,6 +201,8 @@ export class ConsultantSkillsEditComponent implements OnInit, OnDestroy {
   }
 
   selected(event: MatAutocompleteSelectedEvent): void {
+    // Need to strip the selected property from a selected option so that
+    // we don't accidentally duplicate the option in the availableSkills$ observable
     const newSkill = {
       id: event.option.value.id,
       name: event.option.value.name
@@ -187,14 +210,6 @@ export class ConsultantSkillsEditComponent implements OnInit, OnDestroy {
     this._selectedSkills$.next([...this.selectedSkills, newSkill])
     this.skillCtrl.setValue(null);
     this.skillInput.nativeElement.value = '';
-  }
-
-  private filterSkills(name: string): Observable<Skill[]> {
-    const filterName = name.toLowerCase();
-    return this.availableSkills$
-      .pipe(
-        map(skills => skills.filter(skill => skill.name.toLowerCase().indexOf(filterName) === 0))
-      );
   }
 
   updateConsultant() {
@@ -205,28 +220,35 @@ export class ConsultantSkillsEditComponent implements OnInit, OnDestroy {
     if (newSkillsExist) {
       this.updateWithNewSkills();
     } else {
-      // Existing skills here means skills that are in availableSkills, 
-      // not necessarily skills that were already in the consultant object
       this.updateWithExistingSkills();
     }
   }
 
+  private filterSkills(name: string): Observable<Skill[]> {
+    const filterName = name.toLowerCase();
+    return this.availableSkills$
+      .pipe(
+        map(skills => skills.filter(skill => skill.name.toLowerCase().indexOf(filterName) === 0))
+      );
+  }
+
   private updateWithNewSkills() {
-    // Existing skills here means skills that are in availableSkills, 
-    // not necessarily skills that were already in the consultant object
     const existingSkills = this.selectedSkills.filter(skill => skill.id !== null);
     const newSkills = this.selectedSkills.filter(skill => skill.id === null);
     const newSkillRequests = newSkills.map(skill => this.consultantSkillService.addNewSkill(skill.name));
 
     forkJoin(...newSkillRequests)
-      .pipe(switchMap((responseSkills: Skill[]) => {
-        const skills = [
-          ...responseSkills,
-          ...existingSkills
-        ]
+      .pipe(
+        switchMap((responseSkills: Skill[]) => {
+          const skills = [
+            ...responseSkills,
+            ...existingSkills
+          ]
 
-        return this.updateSkills(skills);
-      }))
+          return this.updateSkills(skills);
+        }),
+        takeUntil(this.destroy$)
+      )
       .subscribe(() => this.close());
   }
 
