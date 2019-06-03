15a67e7b7905e4ac5fa0d082e4c9d9b15d635b6b
johnsonbrianna <johnsonbrianna1414@gmail.com>
2019-06-03 15:23:17 -0500

    education

--- src/app/feature/consultant/index.ts
+++ src/app/feature/consultant/index.ts
@@ -9,6 +9,7 @@ export * from './components/shared/consultant-skills-edit/consultant-skills-edit
 export * from './components/shared/consultant-skills-edit/consultant-skills-edit-add-remove/consultant-skills-edit-add-remove.component';
 export * from './components/shared/consultant-skills-edit/consultant-skills-edit-reorder-display/consultant-skills-edit-reorder-display.component';
 export * from './components/consultant-technical-skills/consultant-technical-skills.component';
+export * from './components/consultant-education-certification-edit/consultant-education-certifications-edit.component';
 export * from './components/consultant-education-certifications/consultant-education-certifications.component';
 export * from './components/consultant-education-certification-edit/consultant-education-certifications-edit.component';
 export * from './components/consultant-experience/consultant-experience.component';
